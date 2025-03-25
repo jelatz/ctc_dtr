7917bf8857595c7364d8d680d36ed0a14d637735
ctc_lad <ctc_lad@mail2.calltekcenter.com>
2025-03-25 16:50:19 +0800

    added swiper js

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,16 +1,37 @@
 <template>
   <Head title=" - Home"/>
-  <div class="contianer mx-auto pt-96">
-    <h1 class="text-center text-9xl font-bold animate-bounce">
-      hey
-    </h1>
+  <div class="absolute z-50 top-68 -translate-y-1/2 left-72">
+    <swiper
+    :slides-per-view="3"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    ...
+  </swiper>
   </div>
 </template>
 
 <script setup>
+ // Import Swiper Vue.js components
+ import { Swiper, SwiperSlide } from 'swiper/vue';
 
+// Import Swiper styles
+import 'swiper/css';
+
+// Define the methods
+const onSwiper = (swiper) => {
+  console.log(swiper);
+};
+
+const onSlideChange = () => {
+  console.log('slide change');
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
