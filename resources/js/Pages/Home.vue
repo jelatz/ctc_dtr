30362444d270e17bb27ab40fb97d20f67e4b530c
ctc_lad <ctc_lad@mail2.calltekcenter.com>
2025-03-25 15:51:54 +0800

    initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <Head title=" - Home"/>
+  <div class="contianer mx-auto pt-96">
+    <h1 class="text-center text-9xl font-bold animate-bounce">
+      hey
+    </h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
