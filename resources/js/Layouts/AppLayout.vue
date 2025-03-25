6c416ae53cdc81244c71e6db527a0bebc074532b
Jelatz <lanetejlad@gmail.com>
2025-03-25 22:24:35 +0800

    added the sweeper library

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="relative overflow-hidden h-screen">
         <div
-            class="w-[1200px] h-[1300px] bg-[#fbc04a] rounded-[100px] rotate-[45deg] absolute z-50 -top-40 -left-[30rem]"
-        ></div>
-        <div
-            class="w-[700px] h-[700px] bg-[#fbc04a8c] rounded-[100px] rotate-[45deg] absolute left-[8rem] -top-[9rem]"
-        ></div>
-        <div class="absolute left-[48rem] top-[2rem]">
-            <i class="fa-solid fa-quote-right text-[9rem] text-[#7c7b788c]"></i>
+            class="w-[1200px] h-[1300px] bg-[#fbc04a] rounded-[100px] rotate-[45deg] absolute z-50 -top-40 -left-[30rem]">
+        </div>
+        <div class="w-[700px] h-[700px] bg-[#fbc04a8c] rounded-[100px] rotate-[45deg] absolute left-[8rem] -top-[9rem]">
+        </div>
+        <div class="absolute left-[36rem] z-50 top-[4rem]">
+            <i class="fa-solid fa-quote-right text-[12rem] text-[#7c7b788c]"></i>
         </div>
         <slot />
     </div>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,37 +1,71 @@
 <template>
-  <Head title=" - Home"/>
-  <div class="absolute z-50 top-68 -translate-y-1/2 left-72">
-    <swiper
-    :slides-per-view="3"
-    :space-between="50"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    ...
-  </swiper>
+
+  <Head title=" - Home" />
+  <div class="absolute z-50 top-[35rem] -translate-y-1/2 left-[3rem]">
+    <Swiper :modules="[Autoplay, Navigation, Pagination]" :slides-per-view="1" :space-between="50" :loop="true"
+      :autoplay="{
+        delay: 3000,
+        disableOnInteraction: false
+      }" :pagination="{
+        clickable: true
+      }" :navigation="true" class="mySwiper">
+      <SwiperSlide v-for="(item, index) in items" :key="index">
+        <div class="relative w-[70%] mx-auto">
+          <img :src="item.image" :alt="'Slide ' + (index + 1)" class="w-20 h-20 rounded-full object-cover" />
+          <h3 class="text-xl font-bold">{{ item.title }}</h3>
+          <p>{{ item.description }}</p>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+    <div class="w-[30rem] flex justify-between items-center mt-20">
+      <button class="text-left"><strong>F1</strong><span class="block">Login</span></button>
+      <button class="text-left"><strong>F2</strong><span class="block">Logout</span></button>
+    </div>
   </div>
 </template>
 
 <script setup>
- // Import Swiper Vue.js components
- import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/swiper-bundle.css'
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'
 
-// Import Swiper styles
-import 'swiper/css';
-
-// Define the methods
-const onSwiper = (swiper) => {
-  console.log(swiper);
-};
-
-const onSlideChange = () => {
-  console.log('slide change');
-};
+// Updated data with both images and text
+const items = [
+  {
+    image: '/images/slide1.jpg',
+    title: 'Slide 1 Title',
+    description: 'Short description for slide 1'
+  },
+  {
+    image: '/images/slide2.jpg',
+    title: 'Slide 2 Title',
+    description: 'Short description for slide 2'
+  },
+  {
+    image: '/images/slide3.jpg',
+    title: 'Slide 3 Title',
+    description: 'Short description for slide 3'
+  }
+]
 </script>
 
 <style scoped>
+.mySwiper {
+  width: 50rem;
+  height: 300px;
+}
 
+/* Change color of next button */
+:deep(.swiper-button-next) {
+  color: #000;
+}
+
+/* Change color of prev button */
+:deep(.swiper-button-prev) {
+  color: #000;
+}
+
+:deep(.swiper-pagination-bullet-active) {
+  background: #000;
+}
 </style>
