cbf1da8b3970f1331100fcd4da81d850a30548f6
ctc_lad <ctc_lad@mail2.calltekcenter.com>
2025-08-01 16:48:35 +0800

    removed unecessary lines

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Http\Controllers\DtrController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/', function () {
-//     return Inertia::render('Home');
-// })->name('homepage');
 
 Route::inertia('/', 'Home')->name('home');
-// Route::post('/get-schedules', [DtrController::class, 'getEmployeeAndSchedules'])
-//     ->name('get-schedules');
 Route::match(['get', 'post'], '/get-schedules', [DtrController::class, 'getEmployeeAndSchedules'])->name('get-schedules');
-
-
 Route::post('/confirm-dtr', [DtrController::class, 'addDtr'])->name('confirm-dtr');
