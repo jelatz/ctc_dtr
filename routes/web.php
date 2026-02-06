0c08399fc2a564315010d16ac17e587aea24aa66
ctc_lad <ctc_lad@mail2.calltekcenter.com>
2026-02-06 23:23:17 +0800

    modified the web.php

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DtrController;
+use Inertia\Inertia;
 
-
-Route::inertia('/', 'Home')->name('home');
+Route::get('/', function(){
+  return Inertia::render('Home');
+})->name('home');
 Route::match(['get', 'post'], '/get-schedules', [DtrController::class, 'getEmployeeAndSchedules'])->name('get-schedules');
 Route::post('/confirm-dtr', [DtrController::class, 'addDtr'])->name('confirm-dtr');
